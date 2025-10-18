6af9a8378d47817cb46ed8da0958348f9a1b0b51
Mohamed-Saad288 <mohamed.saadd288@gmail.com>
2025-10-18 18:50:19 +0300

    add

--- app/Modules/Admin/Database/Migrations/2025_09_27_094116_create_home_section_products_table.php
+++ app/Modules/Admin/Database/Migrations/2025_09_27_094116_create_home_section_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('product_variation_id')->constrained("product_variations")->onDelete('cascade');
             $table->integer('sort_order')->default(0);
             $table->timestamps();
-
         });
     }
 
