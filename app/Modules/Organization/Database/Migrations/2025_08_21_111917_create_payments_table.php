e9c160e22f66c2666f27d8655ad71055b116731a
NashaatMohamed <74777475+NashaatMohamed@users.noreply.github.com>
2025-10-22 08:14:02 +0300

    update

--- app/Modules/Organization/Database/Migrations/2025_08_21_111917_create_payments_table.php
+++ app/Modules/Organization/Database/Migrations/2025_08_21_111917_create_payments_table.php
@@ -4,8 +4,7 @@ use App\Modules\Base\Database\Migrations\BaseMigration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends BaseMigration
-{
+return new class extends BaseMigration {
     /**
      * Run the migrations.
      */
@@ -14,14 +13,9 @@ return new class extends BaseMigration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
-            $table->tinyInteger('status')->default(1)
-                ->comment('1=>pending , 2=>completed , 3=>failed , 4=>refunded');
-
+            $table->foreignId("payment_method_id")->constrained('payment_methods')->onDelete('cascade');
+            $table->tinyInteger('status')->default(1)->comment('1=>pending , 2=>completed , 3=>failed , 4=>refunded');
             $table->decimal('amount', 10, 2)->default(0);
-            $table->tinyInteger('method')->default(1)->comment(
-                '1=>cash_on_delivery , 2=>card , 3=> wallet , 4=>bank_transfer , 5=>paypal'
-            );
-            $table->tinyInteger('gateway')->default(1)->comment('1=>stripe , 2=>fawry , 3=>paymob , 4=>paypal');
             $table->string('transaction_id')->nullable();
             $table->string('currency', 10)->default('USD');
             $table->json('meta')->nullable();
